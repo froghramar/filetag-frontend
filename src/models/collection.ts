7edcdc98f2e8c22c0f5a8b722e09b3ba5b15734a
Feroz Ahmmed <froghramar@gmail.com>
2018-03-21 22:33:19 +0600

    add folder models and services

---
+++ src/models/collection.ts
@@ -0,0 +1,22 @@
+export interface ICollection {
+	_id?: string;
+	Directory: string;
+	Name: string;
+	UrlPath: string;
+}
+
+export class Collection implements ICollection {
+	_id?: string;
+	Directory: string;
+	Name: string;
+	UrlPath: string;
+
+	constructor(collection: ICollection) {
+		if (collection._id) {
+			this._id = collection._id;
+		}
+		this.Directory = collection.Directory;
+		this.Name = collection.Name;
+		this.UrlPath = collection.UrlPath;
+	}
+}
