44d71ec93797a2d24ff72cbb8532d4ba5eb80088
Feroz Ahmmed <froghramar@gmail.com>
2018-03-21 17:45:02 +0600

    list of collection

--- src/app/collection-list/collection-list.component.ts
+++ src/app/collection-list/collection-list.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionListComponent implements OnInit {
 
+	collections = [
+		{
+			name: 'Photos',
+			updated: new Date('1/1/16'),
+		},
+		{
+			name: 'Recipes',
+			updated: new Date('1/17/16'),
+		},
+		{
+			name: 'Work',
+			updated: new Date('1/28/16'),
+		}
+	];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/collection/collection.module.ts
+++ src/app/collection/collection.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatFormFieldModule, MatIconModule, MatInputModule, MatListModule } from '@angular/material';
 
 import { CollectionListComponent } from '../collection-list/collection-list.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatInputModule,
+		MatListModule,
 		RouterModule.forChild([
 			{
 				path: '',
