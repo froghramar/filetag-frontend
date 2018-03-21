84414bd822c3fd40967c446b603cb78b3ddef772
Feroz Ahmmed <froghramar@gmail.com>
2018-03-21 14:40:44 +0600

    collapse sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+	isSidenavOpened = true;
+
 	constructor() { }
+
+	toggleSidenav () {
+		this.isSidenavOpened = !this.isSidenavOpened;
+	}
 }
